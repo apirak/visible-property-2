c686d35766b6b262cbf77fec31385c0abe227632
Apirak Panatkool <apirakb@gmail.com>
2023-08-30 22:31:12 +0700

    show local variable

--- src/component/properties.tsx
+++ src/component/properties.tsx
@@ -3,24 +3,22 @@ import { Help, HelpList } from "../module/mockupData";
 import { Property } from "./property";
 import styles from "../style.css";
 
-function createRow(lists:HelpList[]){
-  const list = lists.map(({label, value, api}) => {
-    return (
-      <Property name={label} value={value} api={api} />
-    );
+function createRow(lists: HelpList[]) {
+  const list = lists.map(({ label, value, api }) => {
+    return <Property name={label} value={value} api={api} />;
   });
   return list;
 }
 
-function createTable(data:Help[]|undefined) {
-  if(typeof data !== "undefined"){
-    const header =  data.map(({title, list}) => {
+function createTable(data: Help[] | undefined) {
+  if (typeof data !== "undefined") {
+    const header = data.map(({ title, list }) => {
       return (
         <div>
           <div class={styles.headerStyle}>{title}</div>
           {createRow(list)}
         </div>
-      )
+      );
     });
     return header;
   } else {
@@ -28,12 +26,8 @@ function createTable(data:Help[]|undefined) {
   }
 }
 
-const Properties = (props:{data:Help[]|undefined}) => {
-  return (
-    <div>
-      {createTable(props.data)}
-    </div>
-  )
-}
+const Properties = (props: { data: Help[] | undefined }) => {
+  return <div class={styles.propertiesStyle}>{createTable(props.data)}</div>;
+};
 
-export { Properties }
+export { Properties };
